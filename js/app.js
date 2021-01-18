d7cda0894a630322f4b1611ba4d7952b9d5adb42
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-17 19:44:37 -0800

    display content from fetch response

--- js/app.js
+++ js/app.js
@@ -5,15 +5,32 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+fetch('https://dog.ceo/api/breeds/list')
+    .then(response => response.json())
+    .then(data => /*console.log(data)*/ generateOptions(data.message))
+
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => /*console.log(response)*/ response.json())
-    .then(data => /*console.log(data)*/ console.log(data.message))
+    .then(data => /*console.log(data)*/ /*console.log(data.message)*/ generateImage(data.message))
 
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+function generateOptions(data){
+    const options = data.map(item => ` 
+        <option value='${item}'>${item}</option>
+        `).join('');
+        select.innerHTML = options;
+}
 
+function generateImage(data){
+    const html = `
+        <img src='${data}' alt>
+        <p>Click to view images of ${select.value}s</p>
+    `;
+    card.innerHTML = html;
+}
 
 
 // ------------------------------------------
