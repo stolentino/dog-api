f80c93729f4fe6eb6f7faa6d9156bf888280b197
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-17 21:44:02 -0800

    use promise.all to manage multiple request

--- js/app.js
+++ js/app.js
@@ -5,55 +5,64 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-function fetchData(url){
-    return fetch(url)
-        .then(checkStatus)
-        .then(res => res.json())
-        .catch(error => console.log('Looks like there was a problem!', error))
+
+function fetchData(url) {
+  return fetch(url)
+           .then(checkStatus)  
+           .then(res => res.json())
+           .catch(error => console.log('Looks like there was a problem!', error))
 }
 
-fetchData('https://dog.ceo/api/breeds/listgfgf')
-    .then(data =>  generateOptions(data.message))
-
-fetchData('https://dog.ceo/api/breeds/image/random')
-    .then(data => generateImage(data.message))
+Promise.all([
+  fetchData('https://dog.ceo/api/breeds/list'),
+  fetchData('https://dog.ceo/api/breeds/image/random')  
+])
+.then(data => {
+  const breedList = data[0].message;
+  const randomImage = data[1].message;
+  
+  generateOptions(breedList);
+  generateImage(randomImage);
+})
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-function checkStatus(response){
-    if(response.ok){
-        return Promise.resolve(response);
-    }else{
-        return Promise.reject(new Error(response.statusText));
-    }
-}
-function generateOptions(data){
-    const options = data.map(item => ` 
-        <option value='${item}'>${item}</option>
-        `).join('');
-        select.innerHTML = options;
+
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
 }
 
-function generateImage(data){
-    const html = `
-        <img src='${data}' alt>
-        <p>Click to view images of ${select.value}s</p>
-    `;
-    card.innerHTML = html;
+function generateOptions(data) {
+  const options = data.map(item => `
+    <option value='${item}'>${item}</option>
+  `).join('');
+  select.innerHTML = options;
 }
 
-function fetchBreedImage(){
-    const breed = select.value;
-    const img = card.querySelector('img');
-    const p = card.querySelector('p');
+function generateImage(data) {
+  const html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+  `;
+  card.innerHTML = html;
+}
 
-    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
-        .then(data => {
-            img.src = data.message;
-            img.alt = breed;
-            p.textContent = `Click to view more ${breed}s`;
-        })
+function fetchBreedImage() {
+  const breed = select.value;
+  const img = card.querySelector('img');
+  const p = card.querySelector('p');
+  
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(data => {
+      img.src = data.message;
+      img.alt = breed;
+      p.textContent = `Click to view more ${breed}s`;
+    })
 }
 
 // ------------------------------------------
@@ -62,9 +71,12 @@ function fetchBreedImage(){
 select.addEventListener('change', fetchBreedImage);
 card.addEventListener('click', fetchBreedImage);
 
-
-
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
+
+
+
+
+
 
