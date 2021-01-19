4ac1ad28e10f69424c9ffedea80112edad059cb7
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-18 21:41:35 -0800

    post data with fethapi

--- js/app.js
+++ js/app.js
@@ -70,11 +70,27 @@ function fetchBreedImage() {
 // ------------------------------------------
 select.addEventListener('change', fetchBreedImage);
 card.addEventListener('click', fetchBreedImage);
+form.addEventListener('submit', postData);
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
+function postData(e){
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
 
+  fetch('https://jsonplaceholder.typicode.com/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name, comment })
+  })
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
 
 
 
